d0d75ac8a3543d78ce32ecb879a257bcac31bf0e
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2025-12-27 17:43:15 +0900

    add: indexとshowを作成し、超ミニマムなブログ基盤を作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,10 @@ class ArticlesController < ApplicationController
   allow_unauthenticated_access only: [ :index, :show ]
 
   def index
+    @articles = Article.where(published: true).order(published_at: :desc)
   end
 
   def show
+    @article = Article.find_by!(slug: params[:id])
   end
 end
